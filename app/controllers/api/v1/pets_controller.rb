c7aa63176dae6e9313a955938f53daf672f51999
TCS <caffeinesage44@gmail.com>
2023-02-18 12:55:15 +0000

    Tried to filter information by species, added links to resources.

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -5,10 +5,33 @@ module Api
     class PetsController < ApplicationController
 
       def index
-        @pets = Pet.all
-        render json: @pets, status: :ok
+        
+
+        if params[:species] == "dog"
+
+          @dogs = Pet.find(params[:species] == "dog")
+          render json: @dogs, status: :ok
+
+        elsif params[:species] == "cat"
+
+          @cats = Pet.find(params[:species] == "cat")
+          render json: @cats, status: :ok
+
+        else
+
+          @pets = Pet.all
+          render json: @pets, status: :ok
+        
+        end
       end
+
+      
 
     end
   end
 end
+
+#resources:
+#  https://www.section.io/engineering-education/how-to-integrate-a-react-application-with-rails-api/
+#  https://guides.rubyonrails.org/association_basics.html
+#  https://rootstack.com/en/blog/ruby-on-rails-api
